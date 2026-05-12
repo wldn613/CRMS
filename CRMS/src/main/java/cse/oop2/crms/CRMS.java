7d78903f388833e1127761761fe50345a9c6e6a3
alsrud <mink0502k@gmail.com>
2026-05-12 15:39:36 +0900

    프로젝트 생성

---
+++ CRMS/src/main/java/cse/oop2/crms/CRMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cse.oop2.crms;
+
+/**
+ *
+ * @author mink0
+ */
+public class CRMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
