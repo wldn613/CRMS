2eb9d70bd8171a58f3d21877160cbe082e78a56e
alsrud <mink0502k@gmail.com>
2026-05-13 11:45:43 +0900

    초기화

--- CRMS/src/main/java/cse/oop2/crms/CRMS.java
+++ CRMS/src/main/java/cse/oop2/crms/CRMS.java
@@ -1,16 +1,8 @@
 package cse.oop2.crms;
 
 //import cse.oop2.crms.ui.MainFrame;
-import javax.swing.SwingUtilities;
+
 
 public class CRMS {
-    public static void main(String[] args) {
-        // SFR-601: Java 21 기반 실행 확인
-        System.out.println("강의실 예약 시스템을 시작합니다. (JDK 21)");
-        
-        SwingUtilities.invokeLater(() -> {
-            // 팀원 D가 만들 UI 메인 프레임을 여기서 호출합니다.
-            // new MainFrame().setVisible(true); 
-        });
-    }
+    
 }
